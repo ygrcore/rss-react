0b3be98fb858efed42b7ba0b0ade3a958c1e2610
Egor <itpd2010@yandex.ru>
2023-11-10 15:08:18 +0100

    feat: add logo

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,14 @@
 import Pokedex from '../components/PokeSearchForm/Pokedex';
 import { PokedexProvider } from '../components/PokedexContext/PokedexContext';
 import ErrorBoundary from '../components/errorBoundary/ErrorBoundary';
+import pokemonLogo from '../assets/pokemonLogo.svg';
 
 const MainPage = () => {
   return (
     <>
+      <div>
+        <img src={pokemonLogo} alt="Pokemon Logo" />
+      </div>
       <PokedexProvider>
         <ErrorBoundary>
           <Pokedex />
